96746a34b7fb0b9eab2c79219a01a3e46ea16050
kirska <fr_bernkastel@outlook.com>
2022-10-28 00:40:56 -0700

    add the objects and logics to support the create/draw nodes in the editor

--- Assets/Scripts/NodeGraph/RoomNodeSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeSO.cs
@@ -1,5 +1,6 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class RoomNodeSO : ScriptableObject
@@ -11,4 +12,79 @@ public class RoomNodeSO : ScriptableObject
     [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
 
     public RoomNodeTypeSO roomNodeType;
+
+
+    #region EDITOR_CODE
+    // the following code will be called from the room node graph editor to help process and draw the editor UI/UX.
+#if UNITY_EDITOR
+    [HideInInspector] public Rect rect;
+
+
+    /// <summary>
+    /// Initialize the roomNode when being created, including properties and sprite info
+    /// </summary>
+    /// <param name="rect"></param>
+    /// <param name="nodeGraph"></param>
+    /// <param name="nodeType"></param>
+    public void Initialize(Rect rect, RoomNodeGraphSO nodeGraph, RoomNodeTypeSO nodeType)
+    {
+        this.rect = rect;
+        this.id = Guid.NewGuid().ToString();
+        this.name = "RoomNode";
+        this.roomNodeGraph = nodeGraph;
+        this.roomNodeType = nodeType;
+
+        // Load room node type list
+        this.roomNodeTypeList = nodeGraph.roomNodeTypeList;  // The original code is using - roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
+    }
+
+
+    /// <summary>
+    /// Draw this room node to the editor, based on rect
+    /// </summary>
+    /// <param name="nodeStyle"></param>
+    public void Draw(GUIStyle nodeStyle)
+    {
+        // Draw Node Box Using Begin Area
+        GUILayout.BeginArea(rect, nodeStyle);
+
+        // Start Region To Detect Popup Selection Changes
+        EditorGUI.BeginChangeCheck();
+
+        // Display a popup using the RoomNodeType name values that can be selected from (default to the currently set roomNodeType)
+        int selected = roomNodeTypeList.list.FindIndex(x => x == roomNodeType);
+
+        int selection = EditorGUILayout.Popup("", selected, GetRoomNodeTypesToDisplay());
+
+        roomNodeType = roomNodeTypeList.list[selection];
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            EditorUtility.SetDirty(this);
+        }
+
+        GUILayout.EndArea();
+    }
+
+    /// <summary>
+    /// Return all room types could be displayed for callback.
+    /// </summary>
+    /// <returns></returns>
+    public string[] GetRoomNodeTypesToDisplay()
+    {
+        string[] roomArray = new string[roomNodeTypeList.list.Count];
+
+        for (int i = 0; i < roomNodeTypeList.list.Count; i += 1)
+        {
+            if (roomNodeTypeList.list[i].displayInNodeGraphEditor)
+            {
+                roomArray[i] = roomNodeTypeList.list[i].roomNodeTypeName;
+            }
+        }
+
+        return roomArray;
+    }
+
+#endif
+    #endregion
 }
--- Assets/Scripts/NodeGraph/RoomNodeTypeListSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeTypeListSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "RoomNodeTypeList", menuName = "Scriptable Objects/Dungeon/Room Node Type List")]
+[CreateAssetMenu(fileName = "RoomNodeTypeListSO", menuName = "Scriptable Objects/Dungeon/Room Node Type List")]
 public class RoomNodeTypeListSO : ScriptableObject
 {
     #region Header ROOM NODE TYPE LIST
